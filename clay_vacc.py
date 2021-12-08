56badc34219d4065cb9597a4c0edaa7a4d06928e
claykaufmann <claykaufmann@gmail.com>
2021-12-08 13:26:15 -0500

    add vacc file

---
+++ clay_vacc.py
@@ -0,0 +1,160 @@
+# imports
+from keras.preprocessing.image import ImageDataGenerator, array_to_img,img_to_array,load_img
+import tensorflow.keras as K
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import os
+import glob
+
+# get directories
+main_direc = os.getcwd()
+images_dir = os.path.join(main_direc, 'data/images/images')
+
+# csv location
+artist_csv_loc = os.path.join(main_direc, 'data/artists.csv')   
+
+"""
+Set hyperparams for the number of classes and image generators
+"""
+
+IMG_WIDTH = 299
+IMG_HEIGHT = 299
+BATCH_SIZE = 64
+NUM_ARTISTS = 10 # this is 11 to get to 10 classes, can be changed...
+
+# Collecting Needed Images
+artists = pd.read_csv(artist_csv_loc)
+
+# Creating a dataframe with the top 10 artists by number of paintings
+artists_sort = artists.sort_values(by=['paintings'], ascending=False)
+
+artists_top = artists_sort.head(NUM_ARTISTS)
+print(artists_top)
+
+# Images
+artists_dir = os.listdir(images_dir) # Files are named after each artists
+
+# Images DataFrame
+artists_top_name = artists_top['name'].str.replace(' ', '_').values
+
+images_df = pd.DataFrame()
+for name in artists_top_name:
+    images_df = pd.concat([images_df, pd.DataFrame(data={'Path': glob.glob('data/images/images/' + name + '/*'), 'Name': name})], ignore_index=True)
+
+print(images_df)
+
+train_df = images_df.sample(frac=0.8, random_state=200)
+test_df = images_df.drop(train_df.index)
+
+if K.backend.image_data_format() == 'channels_first':
+    input_shape = (3, IMG_WIDTH, IMG_HEIGHT)
+else:
+    input_shape = (IMG_WIDTH, IMG_HEIGHT, 3)
+
+"""
+Build generators
+"""
+
+train_generator = ImageDataGenerator(rescale=1.0 / 255,
+                                    rotation_range=20,
+                                    zoom_range=0.05,
+                                    width_shift_range=0.05,
+                                    height_shift_range=0.05,
+                                    shear_range=0.05,
+                                    horizontal_flip=True,
+                                    fill_mode="nearest",
+                                    validation_split=0.15,
+                                    preprocessing_function=preprocess_input
+                                    )
+
+test_generator = ImageDataGenerator(rescale=1.0 / 255, preprocessing_function=preprocess_input)
+
+train_gen = train_generator.flow_from_dataframe(
+    train_df,
+    shuffle=True,
+    x_col='Path',
+    y_col='Name',
+    class_mode='categorical',
+    subset="training",
+    batch_size=BATCH_SIZE,
+    target_size=(IMG_WIDTH, IMG_HEIGHT),
+    seed=42
+)
+
+valid_gen = train_generator.flow_from_dataframe(
+    train_df,
+    subset="validation",
+    shuffle=True,
+    x_col='Path',
+    y_col='Name',
+    class_mode='categorical',
+    batch_size=BATCH_SIZE,
+    target_size=(IMG_WIDTH, IMG_HEIGHT),
+    seed=42
+)
+
+test_gen = test_generator.flow_from_dataframe(
+    test_df,
+    x_col='Path',
+    batch_size=1,
+    shuffle=False,
+    class_mode=None,
+    target_size=(IMG_WIDTH, IMG_HEIGHT)
+)
+
+# Set the amount of steps for training, validation, and testing data
+# based on the batch size
+steps_train = train_gen.n//train_gen.batch_size
+steps_valid = valid_gen.n//valid_gen.batch_size
+steps_test = test_gen.n//test_gen.batch_size
+
+"""
+Hyperparameters here:
+"""
+N_EPOCHS = 50
+LEARNING_RATE = 0.001 # 0.001 is the default for Adam set by TensorFlow
+OPTIMIZER = tf.optimizers.Adam(learning_rate=LEARNING_RATE)
+LOSS_FUNCTION = tf.losses.CategoricalCrossentropy(from_logits=False)
+
+# set the input for VGG
+inp = Input(shape=(IMG_HEIGHT,IMG_WIDTH,3))
+
+# load model
+base_model = InceptionV3(include_top=False, input_tensor=inp, pooling='max', weights='imagenet')
+
+# set base model to not be trainable
+base_model.trainable = False
+
+final_model = tf.keras.Sequential()
+final_model.add(base_model)
+
+final_model.add(K.layers.Flatten())
+final_model.add(K.layers.BatchNormalization())
+final_model.add(Dense(256, activation='relu'))
+final_model.add(K.layers.Dropout(0.6))
+final_model.add(Dense(NUM_ARTISTS - 1, activation='softmax'))
+
+final_model.summary()
+
+# compile model
+final_model.compile(
+  optimizer=OPTIMIZER,
+  loss=LOSS_FUNCTION,
+  metrics=['accuracy']
+)
+
+# Fit the model
+final_model.fit_generator(
+    generator = train_gen,
+    steps_per_epoch=steps_train,
+    validation_data = valid_gen,
+    validation_steps = steps_valid,
+    verbose=1,
+    epochs=N_EPOCHS
+)
